dbeadcbf51b1e2418b67fc94a7c1f65a56e57bb1
Brent Gawryluik <brentwg@zoho.com>
2018-02-03 21:15:31 -0800

    Modified spacing...

--- scripts/00_client_tools.sh
+++ scripts/00_client_tools.sh
@@ -6,9 +6,7 @@ wget -q --show-progress --https-only --timestamping \
   https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 
 chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
-
 sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
-
 sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
 
 
@@ -16,7 +14,6 @@ sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
 wget https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/linux/amd64/kubectl
 
 chmod +x kubectl
-
 sudo mv kubectl /usr/local/bin/
 
 
